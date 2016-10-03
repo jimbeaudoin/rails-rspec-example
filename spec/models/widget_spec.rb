6dcb2667560645951087603c470475739f39b409
Jimmy Beaudoin <email@jimmy-beaudoin.com>
2016-10-03 13:17:34 -0400

    Add model test examples for Widget resource

--- spec/models/widget_spec.rb
+++ spec/models/widget_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Widget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has none to begin with" do
+    expect(Widget.count).to eq 0
+  end
+
+  it "has one after adding one" do
+    Widget.create
+    expect(Widget.count).to eq 1
+  end
+
+  it "has none after one was created in a previous example" do
+    expect(Widget.count).to eq 0
+  end
+
+  it "has one after adding one" do
+    instance_double("Widget", name: "my name")
+  end
 end
